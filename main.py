85d808d47463c2bc85eec72425c41c2b471d7770
MGdev14 <giesselmannmaximilian@gmail.com>
2022-03-28 08:51:08 +0200

    init

---
+++ main.py
@@ -0,0 +1,60 @@
+# This is a sample Python script.
+import time
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+import os
+import json
+
+def read_secrets() -> dict:
+    filename = os.path.join('secrets.json')
+    try:
+        with open(filename, mode='r') as f:
+            return json.loads(f.read())
+    except FileNotFoundError:
+        print("ERROR: secrets.json missing")
+        exit(1)
+
+
+def readMailman(URL):
+    page = requests.get(URL)
+    print(page.content)
+    return page.content
+
+
+def parsePage(page):
+    bs = BeautifulSoup(page, 'html.parser')
+    em = bs.find_all('em')
+    print(em[1])
+    count = em[1]
+    count = count.getText()
+    count = count.split(' ')
+    # count = count.split(' ')
+    print("Aktuell " + count[0] + " Empfänger.")
+    return int(count[0])
+
+
+def write_db(timestamp,count):
+    url = 'https://api.mg-orbit.space/mailmanWatch/setSchwimmkursCount.php'
+    ts = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
+    myobj = {'date': ts, 'word_count': count}
+    x = requests.post(url, data=myobj)
+    print(x.text)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    secrets = read_secrets()
+    URL = secrets['adminURL'] + "?adminpw=" + secrets['adminPW']
+    page = readMailman(URL)
+    count = parsePage(page)
+    timestamp = int(time.time())
+    write_db(timestamp, count)
+
+# secrets.json required:
+#
+# {
+#     "adminURL": "",
+#     "adminPW": ""
+#
+# }
